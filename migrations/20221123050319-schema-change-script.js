07142e54691a7461b5b37b9c05d8223de23e7d6a
rohit-raje-786 <rp589006@gmail.com>
2022-11-23 20:44:53 +0530

    file structure changed

--- migrations/20221123050319-schema-change-script.js
+++ migrations/20221123050319-schema-change-script.js
@@ -4,15 +4,13 @@ module.exports = {
     // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
     // Example:
     // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
-    return db.collection("users").updateMany({}, { $set: { branch_name: "" } });
+    return db.collection("users").updateMany({}, { $set: { new_field: "" } });
   },
 
   async down(db, client) {
     // TODO write the statements to rollback your migration (if possible)
     // Example:
     // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
-    return db
-      .collection("users")
-      .updateMany({}, { $unset: { branch_name: "" } });
+    return db.collection("users").updateMany({}, { $unset: { new_field: "" } });
   },
 };
